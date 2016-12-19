91b56bd69b3f2754741d1d8be44d1520b7ecc57e
Alex Bhandari-Young <alexbhandari@gmail.com>
2016-12-18 20:01:22 -0500

    part b mostly working

---
+++ matlab/syndrome_b.m
@@ -0,0 +1,20 @@
+%% syndrome decoder
+%% can decode up to 3 errors
+function c=syndrome_b(r, H, n)
+
+Het = mod(r*transpose(H),2); %(1,n)*(n,11)=(1,11)
+e = [permpos(1,n);permpos(2,n);permpos(3,n)];
+dec_e = ones(1,n)*-1;
+if Het == zeros(1,11)
+    disp('r is correct');
+    dec_e = zeros(1,n);
+else
+for i=1:size(e,1) % loop through error book
+    if Het == mod(e(i,:)*transpose(H),2) %(1,n)*(n,11)=(1,11)
+        dec_e = e(i,:);
+        break;
+    end
+end
+end
+c = abs(r-dec_e);
+end
